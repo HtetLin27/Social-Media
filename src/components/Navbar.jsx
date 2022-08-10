f0adb6549d0e15b2c93081ebbf5f7935aa796bd7
Htet Lin Ko <105052327+HtetLin27@users.noreply.github.com>
2022-08-10 13:23:06 +0630

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,82 @@
-import { AppBar, styled, Toolbar, Typography } from '@mui/material';
-import React from 'react';
+import { Box,AppBar, styled, Toolbar, Typography,InputBase, Badge, Avatar, Menu, MenuItem} from '@mui/material';
+import {React,useState} from 'react';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import EmailIcon from '@mui/icons-material/Email';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const StyledToolbar = styled(Toolbar)({
     display:'flex',
     justifyContent:'space-between'
 })
 const Search = styled("div")(({theme})=>({
-    backgroundColor:'white'
+    backgroundColor:'white',
+    padding:"0 10px",
+    borderRadius:theme.shape.borderRadius,
+    width:"40%"
+}))
+const Icons = styled(Box)(({theme})=>({
+  display:"none",
+  gap:"20px",
+  alignItems:"center",
+  [theme.breakpoints.up("sm")]:{
+    display:"flex"
+  }
+}));
+const UserBox = styled(Box)(({theme})=>({
+  display:"flex",
+  gap:"10px",
+  alignItems:"center",
+  [theme.breakpoints.up("sm")]:{
+    display:"none"
+  }
 }))
 
 function Navbar() {
+  const [open,setOpen] = useState(false)
   return (
     <AppBar position='sticky'>
         <StyledToolbar>
        <Typography variant='h6' sx={{display:{xs:'none',sm:'block'}}}>Social Media</Typography>
        <CatchingPokemonIcon sx={{display:{xs:'block',sm:'none'}}}/>
-       <Search>Search</Search>
+       <Search><InputBase placeholder='seacrch...'/></Search>
+       <Icons>
+        <Badge badgeContent={4} color="error">
+            <EmailIcon/>
+        </Badge>
+        <Badge badgeContent={2} color="error">
+            <NotificationsIcon/>
+        </Badge>
+        <Avatar 
+         sx={{width:30, height:30}}
+         src="https://images.pexels.com/photos/953457/pexels-photo-953457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+         onClick={e=>setOpen(true)}
+         />
+       </Icons>
+       <UserBox   onClick={e=>setOpen(true)}>
+       <Avatar
+        sx={{width:30, height:30}}
+         src="https://images.pexels.com/photos/953457/pexels-photo-953457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+         <Typography variant='span'>Htet</Typography>
+       </UserBox>
         </StyledToolbar>
+        <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e)=>setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem> Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
   )
 }
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 function Rightbar() {
   return (
-    <Box bgcolor="lightcoral"
+    <Box 
+     bgcolor="lightcoral"
      flex={2} p={2} 
      sx={{display:{xs:'none',sm:'block'}}}>Rightbar</Box>
   )
